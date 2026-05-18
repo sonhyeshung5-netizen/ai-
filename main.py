aa042c6bd832f3b764791e0b1754beaded595fbd
sonhyeshung5-netizen <sonhyeshung5@gmail.com>
2026-05-18 10:00:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import streamlit as st
 st.title('나의 첫 웹 서비스 만들기')
-st.text_input('이름이 뭐야')
-st.selectbox('좋아하는게 뭐야',['!','알빠노','물어보지마라','상관없','참께방 위에 순쇠고기페티 둘장 특별한 소스 양상추 치츠 피클 양파 까지'])
-st.button('반갑다')
+a=st.text_input('이름이 뭐야')
+b=st.selectbox('좋아하는게 뭐야',['!','한식','중식','일식','참께방 위에 순쇠고기페티 둘장 특별한 소스 양상추 치츠 피클 양파 까지'])
+if st.button('반갑다'):
+st.write(a+'님,안녕하세요')
+  
